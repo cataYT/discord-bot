3d556fb2d13167cb8a3aa7ab5e05a0a46e9c3843
cataYT <firzt.superb.pro7@gmail.com>
2024-04-14 17:15:35 +0530

    updated intents, created ping and pong thing and message logger

--- main.js
+++ main.js
@@ -1,16 +1,42 @@
-// Require the necessary discord.js classes
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
-// Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildModeration,
+    ],
+});
 
-// When the client is ready, run this code (only once).
-// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
-// It makes some properties non-nullable.
 client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
-// Log in to Discord with your client's token
+client.on(Events.MessageCreate, (message) => {
+    if (message.content === 'ping') {
+        console.log("!")
+        message.reply('Pong!')
+    }
+});
+
+client.on(Events.MessageDelete, (message) => {
+    if (message.content.startsWith("<@") && message.content.endsWith(">")) {
+        message.channel.send(`Message deleted by ${message.author}: no lol not double ping`)
+        console.log("ping delete")
+    } else if (message.embeds || message.attachments) {
+        message.attachments.forEach(element => {
+            if (element.url) {
+                message.channel.send(`Message deleted by ${message.author}: ${element.url}`)
+            }
+        });
+        console.log("embed delete")
+    } else {
+        message.channel.send(`Message deleted by ${message.author}: ${message.content}`)
+        console.log("normal delete")
+    }
+})
+
+  // Log in to Discord with your client's token
 client.login(token);
